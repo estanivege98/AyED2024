614ddc086981fb0199ca3710d968526e64de5bb5
phoenix1998 <estanivegetti@hotmail.com>
2024-03-19 16:54:32 -0300

    Ejercicio 5

--- Practica1/src/ejercicio5/App.java
+++ Practica1/src/ejercicio5/App.java
@@ -1,4 +1,5 @@
 package ejercicio5;
+import java.util.*;
 
 public class App {
     
@@ -17,10 +18,31 @@ public class App {
         return ("Minimo: " + min + " Maximo: " + max +" Promedio: " + prom);
     }
     
+    public static String calculoB(List<Integer> lN){
+        int max = -1, min = 999999, prom, suma = 0;
+        for(int numero : lN){
+            if (numero > max){
+                max = numero;
+            }
+            if (numero < min) {
+                min = numero;
+            }
+            suma += numero;
+        }
+        prom = suma / 4;
+        return ("Minimo: " + min + " Maximo: " + max +" Promedio: " + prom);
+    } 
     
     public static void main(String[] args){
         int[] vecNum = {2, 4, 6, 8};
         System.out.println(calculoA(vecNum));
-
+        List<Integer> listaNum = new ArrayList<Integer>();
+        listaNum.add(2);
+        listaNum.add(4);
+        listaNum.add(6);
+        listaNum.add(8);
+        System.out.println(calculoB(listaNum));
     }
+    
+
 }
