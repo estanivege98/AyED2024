99b52485bd88cd1857e9074955df466e32380947
phoenix1998 <estanivegetti@hotmail.com>
2024-03-21 12:36:08 -0300

    Inciso A implementado

--- Practica1/src/ejercicio7/TestArrayList.java
+++ Practica1/src/ejercicio7/TestArrayList.java
@@ -1,7 +1,25 @@
 package ejercicio7;
 
+import java.util.*;
 public class TestArrayList {
     public static void main(String[] args){
+        /* INCISO A */
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        System.out.println("INCISO A");
+        for (int i = 0; i< args.length; i++){
+            lista.add(Integer.parseInt(args[i]));
+        }
+        System.out.println("Imprimiendo Lista:");
+        for (int i = 0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+        System.out.println();
         
+        /* INCISO D */
+        System.out.println("INCISO D");
+
+
+        in.close();
     }
 }
