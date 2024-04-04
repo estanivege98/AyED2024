3c7e67781bea0769ee91541c9eac872c8cb20578
phoenix1998 <estanivegetti@hotmail.com>
2024-04-04 12:15:28 -0300

    ejercicio 9

---
+++ Practica1/src/ejercicio9/TestBalanceo.java
@@ -0,0 +1,11 @@
+package ejercicio9;
+
+import java.util.Stack;
+
+public class TestBalanceo {
+    public static void mian(String[] args){
+        Stack<String> pila = new Stack<String>();
+        /* Defino las variables */
+        pila.push("{");
+    }
+}
