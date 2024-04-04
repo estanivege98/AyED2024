fe527f7fdea7d3ea0a668ec4fa6f35a6fcddccba
phoenix1998 <estanivegetti@hotmail.com>
2024-04-04 20:48:37 -0300

    Queue implementado

--- Practica1/src/ejercicio8/Queue.java
+++ Practica1/src/ejercicio8/Queue.java
@@ -1,35 +1,35 @@
 package ejercicio8;
 import java.util.*;
-public class Queue <T> {
-    private List <T> data;
+public class Queue <T> extends Sequence{
+    protected List <T> data;
 
     public Queue(){
-        this.data = new ArrayList<>();
+        data = new ArrayList<>();
     }
     public void enqueue(T dato){
-        this.data.add(dato);
+        data.add(dato);
     }
     public T dequeue(){
         T aux = this.data.get(0);
-        this.data.remove(0);
+        data.remove(0);
         return aux;
     }
     public boolean isEmpty(){
-        return (this.data.isEmpty());
+        return (data.isEmpty());
     }
     public T head(){
-        return (this.data.get(0));
+        return (data.get(0));
     }
     public int size(){
-        return (this.data.size());
+        return (data.size());
     }
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Queue: [");
-        for (int i = 0; i < this.data.size(); i++) {
-            sb.append(this.data.get(i));
-            if (i < this.data.size() - 1) {
+        for (int i = 0; i < data.size(); i++) {
+            sb.append(data.get(i));
+            if (i < data.size() - 1) {
                 sb.append(", ");
             }
         }
