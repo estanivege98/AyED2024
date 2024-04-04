1343cf52df0b5d5ca61de7cee2500fdd4a5b71c8
phoenix1998 <estanivegetti@hotmail.com>
2024-04-04 20:48:27 -0300

    DoubleEndedQueue implementado

---
+++ Practica1/src/ejercicio8/DoubleEndedQueue.java
@@ -0,0 +1,12 @@
+package ejercicio8;
+
+public class DoubleEndedQueue<T> extends Queue<T> {
+    
+    public DoubleEndedQueue(){
+        super();
+    }
+
+    public void enqueueFirst(T dato){
+        data.add(dato);
+    }
+}
