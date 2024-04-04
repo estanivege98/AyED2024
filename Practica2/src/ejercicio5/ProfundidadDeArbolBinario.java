ff3456b5e86799f6ef46b79e0eca0757c855fea2
phoenix1998 <estanivegetti@hotmail.com>
2024-04-04 14:27:05 -0300

    Se debe implementar Ejercicio 8 de la practica 1

---
+++ Practica2/src/ejercicio5/ProfundidadDeArbolBinario.java
@@ -0,0 +1,37 @@
+package ejercicio5;
+
+import java.util.LinkedList;
+import java.util.*;
+
+import ejercicio1y2.BinaryTree;
+
+public class ProfundidadDeArbolBinario {
+    private BinaryTree<Integer> arbol;
+    public ProfundidadDeArbolBinario(BinaryTree<Integer> a){
+        this.arbol = a;
+    }
+
+    public int sumaElementosProfundidad(int prof){
+        BinaryTree<Integer> ab = null;
+        Queue<BinaryTree<Integer>> cola = new Queue<BinaryTree<Integer>>();
+        cola.enqueue(this);
+        cola.enqueue(null);
+        while (!cola.isEmpty()) {
+        ab = cola.dequeue();
+        if (ab != null) {
+        System.out.print(ab.getData());
+        if (ab.hasLeftChild()) {
+        cola.enqueue(ab.getLeftChild());
+        }
+        if (ab.hasRightChild()) {
+        cola.enqueue(ab.getRightChild());
+        }
+        } else if (!cola.isEmpty()) {
+        System.out.println();
+        cola.enqueue(null);
+        }
+    }
+ }
+        
+    }
+}
