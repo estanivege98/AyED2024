bf0780b4fb2b18f7c04e82bd4cbcc2600b805448
phoenix1998 <estanivegetti@hotmail.com>
2024-04-04 21:08:12 -0300

    Programa principal ej 5

---
+++ Practica2/src/ejercicio5/TestProfundidadDeArbolBinario.java
@@ -0,0 +1,10 @@
+package ejercicio5;
+
+import ejercicio1y2.BinaryTree;
+
+public class TestProfundidadDeArbolBinario {
+    public static void main(String[] args){
+        BinaryTree<Integer> arbol = new BinaryTree<Integer>();
+        
+    }
+}
