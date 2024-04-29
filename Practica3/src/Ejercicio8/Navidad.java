ef53959572c8d4c4cd0450229ae3486af517cba1
phoenix1998 <estanivegetti@hotmail.com>
2024-04-29 19:41:15 -0300

    Ejercicio 8

--- Practica3/src/Ejercicio8/Navidad.java
+++ Practica3/src/Ejercicio8/Navidad.java
@@ -4,8 +4,29 @@
 
 package Ejercicio8;
 
-public class Navidad {
-    public String esAbetoNavidenio(){
+import Ejercicio1.GeneralTree;
 
+public class Navidad {
+    private GeneralTree<Integer> ab;
+
+    public Navidad(GeneralTree<Integer> arb){
+        this.ab = arb;
+    }
+    public String esAbetoNavidenio(){
+        return (esAbeto(ab) ? "Yes" : "No");
+    }
+    /**
+     * Este método verifica si un árbol es un abeto.
+     * Si un nodo no es una hoja y no es un abeto, retorna falso.
+     * @param ab El árbol a verificar
+     * @return true si es un abeto, false si no lo es
+     */
+    public boolean esAbeto(GeneralTree<Integer> ab){
+        int cantHoja = 0;
+        for(GeneralTree<Integer> h : ab.getChildren()){
+            if(h.isLeaf()) cantHoja++;
+            else if(!esAbeto(h)) return false;
+        }
+        return cantHoja >= 3;
     }
 }
