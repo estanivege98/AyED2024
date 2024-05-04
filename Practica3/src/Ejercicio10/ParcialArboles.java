a9e1aaa226f7b6965ee763bd8f7475eca00ff5f0
phoenix1998 <estanivegetti@hotmail.com>
2024-05-04 12:58:18 -0300

    Ejercicio 10 implementado

--- Practica3/src/Ejercicio10/ParcialArboles.java
+++ Practica3/src/Ejercicio10/ParcialArboles.java
@@ -5,11 +5,23 @@ import java.util.*;
 import Ejercicio1.GeneralTree;
 
 public class ParcialArboles {
-    public static List<Integer> resolver(GeneralTree<Integer> arbol){
+    public static Integer resolver(GeneralTree<Integer> arbol){
         // Pseudocodigo (muy parecido al 7)
         /* preguntar si el arbol es vacio
          * si no es vacio, entonces creo las listas y variables y llamo al helper
          */
+        if(arbol.isLeaf()){
+            return null;
+        }
+        else{
+            List<Integer> listaAct = new LinkedList<Integer>();
+            List<Integer> listaMax = new LinkedList<Integer>();
+            int nivelAct = 0;
+            //int nivelMax = Integer.MIN_VALUE;
+            int sumaMax = Integer.MIN_VALUE;
+            int sumaAct = 0;
+            return resolverHelper(arbol, listaAct, listaMax, nivelAct, sumaAct, sumaMax);
+        }
     }
     private static Integer resolverHelper(GeneralTree<Integer> ab, List<Integer> listaAct, List<Integer> listaMax, int nivelAct,
     int sumaAct, int sumaMax){
@@ -29,5 +41,28 @@ public class ParcialArboles {
             }   
             return sumaMax
         } */
+        if (ab.getData() == 1){
+            listaAct.add(ab.getData()*nivelAct);
+            sumaAct += ab.getData()*nivelAct;
+        }
+        if(ab.isLeaf()){
+            if(sumaAct > sumaMax){
+                sumaMax = sumaAct;
+                sumaAct = 0;
+                listaMax.removeAll(listaMax);
+                listaMax.addAll(listaAct);
+            }
+            
+        }
+        else{
+           for (GeneralTree<Integer> child : ab.getChildren()){
+            sumaMax = resolverHelper(child, listaAct, listaMax, nivelAct+1, sumaAct, sumaMax);
+            if (!listaAct.isEmpty()){
+                listaAct.remove(listaAct.size()-1);
+            }
+            
+            }
+        }
+        return sumaMax;
     } 
 }
