089c4c17bbf4d55a838b6b73361819ab14c2037e
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-06-09 06:17:54 -0300

    Ejercicio 2 implementado

--- Practica5/src/Ejercicio2/Recorridos.java
+++ Practica5/src/Ejercicio2/Recorridos.java
@@ -1,13 +1,59 @@
 package Ejercicio2;
 import java.util.*;
 import ejercicio1.*;
+import Cola.Queue;
 public class Recorridos<T> {
 
-    public List<T> dfs(Graph<T> grafo, Vertex<T> vertice, boolean[] visitados, List<T> camino){
-        visitados[vertice.getPosition()] = true;
-        camino.addLast(vertice.getData());
-        List<Edge<T>> adyacentes = grafo.getEdges(vertice);
-
+    public List<T> dfs(Graph<T> grafo){
+        List<T> camino = new LinkedList<T>();
+        boolean[] visitados = new boolean[grafo.getSize()];
+        for (Vertex<T> vertice : grafo.getVertices()) {
+            if(!visitados[vertice.getPosition()]){
+                dfsHelper(grafo, vertice, visitados, camino);
+            }
+        }
         return camino;
+    }
+
+    private void dfsHelper(Graph<T> grafo, Vertex<T> vertice, boolean[] visitados, List<T> camino){
+        camino.addLast(vertice.getData());
+        visitados[vertice.getPosition()] = true;
+        List<Edge<T>> adyacentes = grafo.getEdges(vertice);
+        for (Edge<T> edge : adyacentes) {
+            Vertex<T> destino = edge.getTarget();
+            if(!visitados[destino.getPosition()]){
+                dfsHelper(grafo, destino, visitados, camino);
+            }
+        }
+
+    }
+
+    public List<T> bfs(Graph<T> grafo){
+        List<T> camino = new LinkedList<T>();
+        boolean[] visitados = new boolean[grafo.getSize()];
+        for (Vertex<T> vertice : grafo.getVertices()) {
+            if(!visitados[vertice.getPosition()]){
+                bfsHelper(grafo, vertice, visitados, camino);
+            }
+        }
+        return camino;
+    }
+
+    private void bfsHelper(Graph<T> grafo, Vertex<T> vertice, boolean[] visitados, List<T> camino){
+        Queue<Vertex<T>> cola = new Queue<>();
+        cola.enqueue(vertice);
+        visitados[vertice.getPosition()] = true;
+        while (!cola.isEmpty()) {
+            Vertex<T> actual = cola.dequeue();
+            camino.addLast(actual.getData());
+            List<Edge<T>> adyacentes = grafo.getEdges(actual);
+            for (Edge<T> edge : adyacentes) {
+                Vertex<T> destino = edge.getTarget();
+                if(!visitados[destino.getPosition()]){
+                    cola.enqueue(destino);
+                    visitados[destino.getPosition()] = true;
+                }
+            }
+        }
     }
 }
